6501bb1015aef728bec5a02d8c9a4ba64fff5ce9
Phil Hanna <ph1204@gmail.com>
2023-01-27 13:28:39 -0500

    Initial commit

---
+++ util.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"time"
+)
+
+// sleep_milli sleeps for the specified number of milliseconds
+func sleep_milli(millis int) {
+	n := time.Duration(millis) * time.Millisecond
+	time.Sleep(n)
+}
---
+++ util_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSleepMilli(t *testing.T) {
+	sleep_milli(45)
+}
