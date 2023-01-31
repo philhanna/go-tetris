067a35500fe83bcb7bc3cb07f1e2ca7a0511e372
Phil Hanna <ph1204@gmail.com>
2023-01-31 16:01:44 -0500

    Wrote tetris.h.go

---
+++ tetris.h.go
@@ -0,0 +1,16 @@
+package tetris
+
+const (
+	// How many cells in a tetromino?
+	TETRIS = 4
+
+	// How many tetrominos?
+	NUM_TETROMINOS = 7
+
+	// How many orientations of a tetromino?
+	NUM_ORIENTATIONS = 4
+
+	// Level constants.
+	MAX_LEVEL       = 19
+	LINES_PER_LEVEL = 10
+)
