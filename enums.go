01180f68cc4f51530437fd53ca39c956efe83398
Phil Hanna <ph1204@gmail.com>
2023-01-31 21:23:07 -0500

    Split TetrisType into its own file

--- enums.go
+++ enums.go
@@ -1,44 +1,5 @@
 package tetris
 
-// ---------------------------------------------------------------------
-//                       Enumerations
-// ---------------------------------------------------------------------
-
-// TetrisType is an enumeration of the type/shape of a tetromino, not
-// including orientation.
-type TetrisType uint8
-
-const (
-	TET_I TetrisType = iota
-	TET_J
-	TET_L
-	TET_O
-	TET_S
-	TET_T
-	TET_Z
-)
-
-// String returns a string representation of a tetris type
-func (t TetrisType) String() string {
-	switch t {
-	case TET_I:
-		return "I"
-	case TET_J:
-		return "J"
-	case TET_L:
-		return "L"
-	case TET_O:
-		return "O"
-	case TET_S:
-		return "S"
-	case TET_T:
-		return "T"
-	case TET_Z:
-		return "Z"
-	}
-	return ""
-}
-
 // TetrisCell is an enumeration of 1x1 blocks within a tetris board.
 type TetrisCell uint8
 
