c99f62032efe0cd3b8529db9b180ec5d68522ba1
Phil Hanna <ph1204@gmail.com>
2023-01-31 21:19:44 -0500

    Added unit tests

--- enums_test.go
+++ enums_test.go
@@ -10,6 +10,12 @@ func TestTetrisType_String(t *testing.T) {
 	}{
 		{"I", TET_I, "I"},
 		{"J", TET_J, "J"},
+		{"L", TET_L, "L"},
+		{"O", TET_O, "O"},
+		{"S", TET_S, "S"},
+		{"T", TET_T, "T"},
+		{"Z", TET_Z, "Z"},
+		{"BOGUS", 86, ""},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
