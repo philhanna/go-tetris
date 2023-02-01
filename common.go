fa8bfe13e42f79d4b03614dc4d2d3116d4ec420d
Phil Hanna <ph1204@gmail.com>
2023-01-31 20:55:45 -0500

    Added comments to common constants

--- common.go
+++ common.go
@@ -1,16 +1,16 @@
 package tetris
 
-const (
-	// How many cells in a tetromino?
-	TETRIS = 4
+// Number of cells in a tetromino
+const NUM_CELLS = 4
 
-	// How many tetrominos?
-	NUM_TETROMINOS = 7
+// Number of orientations of a tetromino
+const NUM_ORIENTATIONS = 4
 
-	// How many orientations of a tetromino?
-	NUM_ORIENTATIONS = 4
+// Number of tetrominos
+const NUM_TETROMINOS = 7
 
-	// Level constants.
-	MAX_LEVEL       = 19
-	LINES_PER_LEVEL = 10
-)
+// Highest level
+const MAX_LEVEL = 19
+
+// Number of lines per level
+const LINES_PER_LEVEL = 10
