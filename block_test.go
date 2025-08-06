0b5c135cf79262763bc59ea608fbb94b27af7f98
Phil Hanna <ph1204@gmail.com>
2025-08-06 10:44:43 -0400

    Added unit test

--- block_test.go
+++ block_test.go
@@ -2,10 +2,13 @@ package tetris
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBlock_String(t *testing.T) {
 	var block Block
 	block.BlockType = TET_J
 	block.Orientation = 35
+	assert.NotEmpty(t, block)
 }
